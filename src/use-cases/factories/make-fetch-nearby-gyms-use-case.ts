c6ad104524b121bb9e9f65cff6e233b5097b1da8
marcosmatasousa <marcosdamata2000@gmail.com>
2026-01-06 22:03:05 -0300

    fixing typos

--- src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
+++ src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
@@ -1,7 +1,7 @@
 import { FetchNearbyGymsUseCase } from "../fetch-nearby-gyms";
 import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository";
 
-export function makeGetUserProfileUseCase() {
+export function makeFetchNearbyGymsUseCase() {
   const gymsRepository = new PrismaGymsRepository();
   const useCase = new FetchNearbyGymsUseCase(gymsRepository);
 
--- src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
+++ src/use-cases/factories/make-fetch-user-check-ins-history-use-case.ts
@@ -1,9 +1,7 @@
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import { GetUserProfileUseCase } from "../get-user-profile";
 import { FetchUserCheckInsHistoryUseCase } from "../fetch-user-check-ins-history";
 import { PrismaCheckInsRepository } from "@/repositories/prisma/prisma-check-ins-repository";
 
-export function makeGetUserProfileUseCase() {
+export function makeFetchUserCheckInHistoryUseCase() {
   const checkinsRepository = new PrismaCheckInsRepository();
   const useCase = new FetchUserCheckInsHistoryUseCase(checkinsRepository);
 
