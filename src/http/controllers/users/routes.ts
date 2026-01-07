738ce9be937567bdd2f9d58a7b6f50362497a301
marcosmatasousa <marcosdamata2000@gmail.com>
2026-01-07 19:16:49 -0300

    implemented refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { profile } from "./profile";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
 import { verifyJWT } from "../../middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.patch("/token/refresh", refresh);
 
   //
   app.get("/me", { onRequest: [verifyJWT] }, profile);
