4487547d6fbc9b5ebb63a847ae7e683f1d484ded
marcosmatasousa <marcosdamata2000@gmail.com>
2026-01-18 14:38:22 -0300

    implemented role based authorization (member and admin)

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -19,7 +19,7 @@ export async function authenticate(
     const { user } = await authenticateUseCase.execute({ email, password });
 
     const token = await reply.jwtSign(
-      {},
+      { role: user.role },
       {
         sign: {
           sub: user.id,
@@ -28,7 +28,7 @@ export async function authenticate(
     );
 
     const refreshToken = await reply.jwtSign(
-      {},
+      { role: user.role },
       {
         sign: {
           sub: user.id,
