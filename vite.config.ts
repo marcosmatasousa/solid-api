86f4ff9b8f90aaf1c9575a877fb851d19978f035
marcosmatasousa <marcosdamata2000@gmail.com>
2026-01-20 13:07:26 -0300

    fixed tests running on build directory

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  test: {
+    exclude: ["build", "dist", "node_modules"],
+  },
   plugins: [tsconfigPaths()],
 });
